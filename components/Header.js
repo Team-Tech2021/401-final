14c03206cdc2b77ea0c486ba4e81fb204f43a56f
moh-ash96 <moh.ash96@gmail.com>
2021-07-12 17:41:44 +0300

    code editor with new setup

--- components/Header.js
+++ components/Header.js
@@ -1,27 +1,17 @@
-// export default function Header(props) {
-//     return (
-//         <header className="bg--300 text-4xl mb-8 ... pt-6 ... pb-8 ...">
-//             <h1 className="ml-4 ...">{props.header}</h1>
-//         </header>
-//     )
-// }
-
-
-
 export default function Header(props) {
     return (
 <header>
     <nav className="bg-white shadow">
-        <div className="container mx-auto px-6 py-3 ">
+        <div className="container px-6 py-3 mx-auto ">
             <div className="md:flex md:items-center md:justify-between">
-                <div className="flex justify-between items-center">
+                <div className="flex items-center justify-between">
                     <div className="text-xl font-semibold text-gray-700">
-                        <a href="#" className="text-gray-800 text-xl font-bold hover:text-gray-700 md:text-2xl">Logo</a>
+                        <a href="#" className="text-xl font-bold text-gray-800 hover:text-gray-700 md:text-2xl">Logo</a>
                     </div>
 
                     <div className="flex md:hidden">
                         <button type="button" className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
-                            <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+                            <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
                                 <path fillRule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
                             </svg>
                         </button>
@@ -29,10 +19,10 @@ export default function Header(props) {
                 </div>
 
                 <div className="hidden -mx-4 md:flex md:items-center">
-                    <a href="#" className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Whiteboard</a>
-                    <a href="#" className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Code Editor </a>
-                    <a href="#" className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Questions </a>
-                    <a href="#" className="block mx-4 mt-2 md:mt-0 text-sm text-gray-700 capitalize hover:text-blue-600">Contact</a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize md:mt-0 hover:text-blue-600">Whiteboard</a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize md:mt-0 hover:text-blue-600">Code Editor </a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize md:mt-0 hover:text-blue-600">Questions </a>
+                    <a href="#" className="block mx-4 mt-2 text-sm text-gray-700 capitalize md:mt-0 hover:text-blue-600">Contact</a>
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 import Main from '../components/Main'
-import ProblemsList from '../components/ProblemsList'
 import Footer from '../components/Footer'
-
-import {fetchAll} from '../services/problems-fetcher'
 import { useState } from 'react'
 
 
-export default function Home({problems}) {
+export default function Home() {
 
   const [title,setTitle] = useState('kuro space');
   return (
@@ -19,17 +16,7 @@ export default function Home({problems}) {
 
       <Header header={title}/>
       <Main title={title} />
-      <ProblemsList problems={problems} />
       <Footer />
     </>
   )
-}
-export async function getStaticProps() {
-  const problems = await fetchAll();
-
-  return {
-      props: { problems },
-      revalidate: 1,
-  }
-
 }
