f7f1a6e769fcfd11b78112cf37ad85eee83d098f
moh-ash96 <moh.ash96@gmail.com>
2021-07-13 16:36:07 +0300

    reimported problem list to main

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,29 @@ import Main from '../components/Main'
 import Footer from '../components/Footer'
 import Link from 'next/dist/client/link'
 import { useState } from 'react'
+import { fetchAll } from '../services/problems-fetcher'
+import ProblemsList from '../components/ProblemsList'
 
 
-export default function Home() {
-
+export default function Home({problems}) {
   const [title,setTitle] = useState('kuro space');
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-
       <Header header={title}/>
-      <Link href="/timer" className="text-right"><a>Timer</a></Link>
+      <Link href="/timer"><a>Timer</a></Link>
       <Main title={title} />
+      <ProblemsList problems={problems} />
       <Footer />
     </>
   )
+}
+export async function getStaticProps() {
+  const problems = await fetchAll();
+  return {
+      props: { problems },
+      revalidate: 1,
+  }
 }
